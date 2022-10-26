97d2c4d43d2ed0092214183ed1ed3a72c9abe77d
Stephanie Pino <stephanievictoria.pino@gmail.com>
2022-10-26 18:54:36 +0200

    modif commentaires rateLimit modif gitignore

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
-	max: 5, // Limit each IP to 3 requests per `window`
+	max: 5, // Limit each IP to 5 requests per `window`
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
